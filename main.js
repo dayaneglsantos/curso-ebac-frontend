bbf7ff10f154281476faaf3da4a5320b0fd773a6
Dayane Santos <dayane0204@hotmail.com>
2023-07-03 17:09:19 -0300

    Exercicio

--- main.js
+++ main.js
@@ -1,44 +1,21 @@
-$(document).ready(function () {
-  $("#cep").mask("00000-000");
-  $("#btn-busca-cep").click(function () {
-    const cep = $("#cep").val();
-    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-    const botao = $(this);
+document.addEventListener("DOMContentLoaded", function () {
+  const xhttp = new XMLHttpRequest();
+  const endpoint = "https://api.github.com/users/ogiansouza";
 
-    $(botao).find("i").addClass("d-none");
-    $(botao).find("span").removeClass("d-none");
+  fetch(endpoint)
+    .then(function (resposta) {
+      return resposta.json();
+    })
+    .then(function (json) {
+      const followers = json.followers;
+      const following = json.following;
+      const rep = json.public_repos;
 
-    // $.ajax(endpoint).done(function (resposta) {
-    //   const logradouro = resposta.logradouro;
-    //   const bairro = resposta.bairro;
-    //   const cidade = resposta.cidade;
-    //   const estado = resposta.estado;
-    //   const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-
-    //   $("#endereco").val(endereco);
-
-    //   setTimeout(function () {
-    //     $(botao).find("i").removeClass("d-none");
-    //     $(botao).find("span").addClass("d-none");
-    //   }, 8000);
-    // });
-
-    fetch(endpoint)
-      .then(function (resposta) {
-        return resposta.json();
-      })
-      .then(function (json) {
-        const logradouro = json.logradouro;
-        const bairro = json.bairro;
-        const cidade = json.cidade;
-        const estado = json.estado;
-        const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-        $("#endereco").val(endereco);
-
-        setTimeout(function () {
-          $(botao).find("i").removeClass("d-none");
-          $(botao).find("span").addClass("d-none");
-        }, 8000);
-      });
-  });
+      const repositorios = (document.getElementById("repositorios").innerText =
+        rep);
+      const seguidores = (document.getElementById("seguidores").innerText =
+        followers);
+      const seguindo = (document.getElementById("seguindo").innerText =
+        following);
+    });
 });
