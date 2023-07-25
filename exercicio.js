dbf749b17996372a647e57b70af3cdeb851c839d
Dayane Santos <dayane0204@hotmail.com>
2023-07-25 10:35:08 -0300

    Commit inicial

---
+++ exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const soma = (num1, num2) => num1 * num2;
+const dizOi = (nome) => `Olá ${nome}`;
+soma(3, 6);
+dizOi('Dayane');
---
+++ exercicio.ts
@@ -0,0 +1,6 @@
+const soma = (num1: number, num2: number):number => num1 * num2;
+
+const dizOi = (nome: string): string => `Olá ${nome}`
+
+soma(3,6)
+dizOi('Dayane')
